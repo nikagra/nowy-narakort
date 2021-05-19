3c5090b53e03f5115f4e73bdc86a4e7d3665d634
Mikita Hradovich <nikagra@gmail.com>
2021-05-19 09:45:19 +0200

    Initial implementation of the Air Quality service client. Using dotenv for storing Slack keys. A number of improvements across the service

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,29 @@
-console.log('Hello world!')
+import "./utils/env";
+import {App, LogLevel, SlackCommandMiddlewareArgs} from '@slack/bolt';
+import Client from "./clients/air-quality"
+
+// Initializes your app with your bot token and signing secret
+const app = new App({
+    token: process.env.SLACK_BOT_TOKEN,
+    signingSecret: process.env.SLACK_SIGNING_SECRET,
+    logLevel: LogLevel.DEBUG
+});
+
+const client = new Client();
+
+app.command("/air", async ({command, ack, say}: SlackCommandMiddlewareArgs) => {
+    console.log(command);
+    await ack();
+
+    let response = await client.getClosestStation(command.text)
+    console.log(response);
+
+    await say(`Station ${response.stationName} with id ${response.id} is the closest station 🌡`);
+});
+
+(async () => {
+    // Start your app
+    await app.start(Number(process.env.PORT) || 3000);
+
+    console.log('⚡️ Bolt app is running!');
+})();
---
+++ src/errors.ts
@@ -0,0 +1,5 @@
+export class InvalidCommandParamsError extends Error {
+}
+
+export class UnableToRetrieveStationsError extends Error {
+}
